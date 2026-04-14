d758379ecddbc88142598beb1e905ab9daa10da7
Wolfe-Jam <wolfejam2020@gmail.com>
2026-04-14 01:28:25 -0400

    chore: Upgrade faf-cli dependency from node@20 to node@22 (LTS)

--- Formula/faf-cli.rb
+++ Formula/faf-cli.rb
@@ -5,7 +5,7 @@ class FafCli < Formula
   sha256 "7fe25fad1f8cb5733db9b1788ae27d2d8f81ab552d5f7ee33b24f1176d938e17"
   license "MIT"
 
-  depends_on "node@20"
+  depends_on "node@22"
 
   def install
     system "npm", "install", "--prefix", libexec, "-g", "faf-cli@#{version}"
